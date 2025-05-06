e71c5f4f003ec845838ecf18fd114e14b8a7219a
Devmultiapisoft <dev.multiapisoft@gmail.com>
2025-05-06 15:20:31 +0530

    pprt changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 7050, // Different port from the admin app
+    open: true
+  },
+  build: {
+    outDir: 'dist' // Output directory for the build
+  }
 })
